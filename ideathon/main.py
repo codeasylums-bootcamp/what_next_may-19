2831c4dc26ab8c37a0fb4222fe022cb0551c05fb
Shubhangi <39852472+Shubhangi105@users.noreply.github.com>
2019-05-26 19:51:10 +0530

    Update main.py

--- ideathon/main.py
+++ ideathon/main.py
@@ -10,35 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'cr
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    phone = db.Column(db.String(60), unique=True)
-    CareerObjective = db.Column(db.String(120), unique=True)
-    Education = db.Column(db.String(80), unique=True)
-    Skills= db.Column(db.String(80), unique=True)
-
-
-def __init__(self, name, email, phone, CareerObjective, Education, Skills):
-    self.name = name
-    self.email = email
-    self.phone = phone
-    self.CareerObjective = CareerObjective
-    self.Education = Education
-    self.Skills = Skills
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('name', 'email', 'phone', 'CareerObjective', 'Education', 'Skills')
-
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
-
 class User2(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
@@ -94,85 +65,13 @@ users_schema4 = User4Schema(many=True)
 def main():
     return render_template("homepage.html")
 
-@app.route("/resume", methods=['GET'])
-def resume():
-    return render_template("templateoh.html")
+
 
 @app.route("/certificate", methods=['GET'])
 def certificate():
     return render_template("temp.html")
 
 
-@app.route("/resume/<string:template>", methods=['GET'])
-def templateNumber(template):
-    if(template=='template1'):
-        
-        return render_template("")
-
-    elif(template=='template2'):
-       
-        return render_template("homepage.html")
-
-    elif(template=='template3'):
-        
-        return render_template("result.html")
-   
-        
-@app.route("/resume/template1/finish",methods=['GET','POST'])
-def fin():
-    if request.method=='POST':
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-        CareerObjective = request.form['CareerObjective']
-        Education = request.form['Education']
-        Skills = request.form['Skills']
-    
-        new_user = User(name, email, phone, CareerObjective, Education, Skills)
-
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template("dummy.html", name, email, phone, CareerObjective,Education, Skills)
-    else:
-        return render_template("dummy.html",name, email, phone, CareerObjective,Education, Skills)
-
-
-@app.route("/resume/template2/finish",methods=['GET','POST'])
-def fin1():
-    if request.method=='POST':
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-        CareerObjective = request.form['CareerObjective']
-        Education = request.form['Education']
-        Skills = request.form['Skills']
-    
-        new_user = User(name=name, email=email,phone= phone,CareerObjective= CareerObjective, Education=Education, Skills=Skills)
-
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template("dummy2.html", name, email, phone, CareerObjective, Education, Skills)
-    else:
-        return render_template("dummy.html",name, email, phone, CareerObjective, Education, Skills)
-
-@app.route("/resume/template3/finish",methods=["GET"])
-def fin2():
-    if request.method=='POST':
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-        CareerObjective = request.form['CareerObjective']
-        Education = request.form['Education']
-        Skills = request.form['Skills']
-    
-        new_user = User(name=name, email=email,phone=phone, CareerObjective=CareerObjective, Education=Education, Skills=Skills)
-
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template("dummy2.html",name, email, phone, CareerObjective, Education, Skills)
-    else:
-        return render_template("dummy.html",name, email, phone, CareerObjective, Education, Skills)
-    
 
 
 @app.route("/certificate/<string:style>", methods=['GET'])
